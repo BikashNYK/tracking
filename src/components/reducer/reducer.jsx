11343f4e8501b3d6cda7f0be2b60679f48d1135b
Bikash Kumar Nayak <fluxtrn@gmail.com>
2023-04-02 13:59:22 +0530

    done

--- src/components/reducer/reducer.jsx
+++ src/components/reducer/reducer.jsx
@@ -1,45 +1,45 @@
 import { ADD_PROJECT, ADD_TASK, STOP_TIMER } from "../actionType/actionType";
 
 const initialState = {
-    projects : [],
-    tasks : [],
-}
+    projects: [],
+    tasks: [],
+};
 
-export const projectReducer = (state = initialState , action)=>{
-    const {type,payload} = action
-    switch (type){
-        case ADD_PROJECT : 
-        return {
-            ...state,
-            projects : [...state.projects,payload],
-        }
-        case ADD_TASK : 
-        return {
-            ...state,
-            tasks: [...state.tasks,payload],
-        }
+export const projectReducer = (state = initialState, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case ADD_PROJECT:
+            return {
+                ...state,
+                projects: [...state.projects, payload],
+            };
+        case ADD_TASK:
+            return {
+                ...state,
+                tasks: [...state.tasks, payload],
+            };
         case STOP_TIMER:
             const currentTask = state.tasks.findIndex((task) => task.id === payload.taskId);
-            if (currentTask === -1){
-                return
+            if (currentTask === -1) {
+                return;
             }
-            const task = state.tasks[currentTask]
-            if(task.stopTime !== null){
-                alert('Task Already Stopped')
-                    return
+            const task = state.tasks[currentTask];
+            if (task.stopTime !== null) {
+                alert('Task Already Stopped');
+                return;
             }
-            const now = new Date()
+            const now = new Date();
             const updatedTask = {
                 ...task,
-                stopTime : now
-            }
-            const updatedTasks = [...state.tasks]
-            updatedTasks[currentTask] = updatedTask
+                stopTime: now
+            };
+            const updatedTasks = [...state.tasks];
+            updatedTasks[currentTask] = updatedTask;
             return {
                 ...state,
                 tasks: updatedTasks
             };
-        default : 
-        return state
+        default:
+            return state;
     }
-}
+};
